e84a23279f35337a2a25c8e15da16337a052043f
Ramzey Burdette <127152283+foxwithahdie@users.noreply.github.com>
2023-05-30 14:07:30 +0000

    playerkey sprite created

--- Rhythm Game Real/rhythmgameattempt2.py
+++ Rhythm Game Real/rhythmgameattempt2.py
@@ -6,11 +6,21 @@ import math
 from pygame import mixer as mix
 import sys
 
+pyg.display.init()
 
+display = pyg.display.set_mode((800, 600))
+width = display.get_width()
+height = display.get_height()
+clock = pyg.time.Clock()
 
-
-
-
+class PlayerKey(pyg.sprite.Sprite):
+    def __init__(self, x, y, rect, filename, button):
+        self.x = x
+        self.y = y #need to create a variable with a sort of section. then further subtract from it (PERCENTAGE)
+        image = pyg.image.load(filename + ".png").convert()
+        rect = image.get_rect()
+        self.button = button
+    def c
 # window
 while True:
     for event in pyg.event.get():
